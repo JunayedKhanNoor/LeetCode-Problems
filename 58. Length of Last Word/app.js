955935302fcfbb49305e3f8446f6db9735bc8bce
JunayedKhanNoor <78534254+JunayedKhanNoor@users.noreply.github.com>
2022-02-03 22:29:56 +0600

    Update app.js

--- 58. Length of Last Word/app.js
+++ 58. Length of Last Word/app.js
@@ -1,1 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 
+ var lengthOfLastWord = function(s) {
+	let c="";
+	let p="";
+	 for (let i = s.length-1; i>=0; i--){
+		 if(p!=="" && c===""){
+			 break;
+		 }else if(s[i]!==" "){
+			 c+=s[i];
+		 } else{
+			 p=c;
+			 c="";
+		 }
+	 }
+	 if(p!==""){
+		 console.log(p.length);
+	 }else{
+		 console.log(c.length);
+	 }
+
+    
+};
+let s = "Hello world  ";
+lengthOfLastWord(s);
